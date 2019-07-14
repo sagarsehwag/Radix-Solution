29617dd1658e263d8c0e4d6ac8167827d444c5e5
Sagar Sehwag <isagarsehwag@gmail.com>
2019-07-14 17:08:55 +0530

    Config Middleware Update
    Signed-off-by: Sagar Sehwag <isagarsehwag@gmail.com>

--- startup/config.js
+++ startup/config.js
@@ -4,4 +4,12 @@ module.exports = function() {
 	if (!config.get("jwtSecret")) {
 		throw new Error("jwtSecret is not defined.");
 	}
+
+	if (!config.get("mongoURI")) {
+		throw new Error("mongoURI is not defined.");
+	}
+
+	if (!config.get("port")) {
+		throw new Error("port is not defined.");
+	}
 };
