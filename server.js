ecfc1797bd429778f632568e583e26d940dc79bd
Sagar Sehwag <isagarsehwag@gmail.com>
2019-09-04 13:04:59 +0530

    Minor Production Correction
    Signed-off-by: Sagar Sehwag <isagarsehwag@gmail.com>

--- server.js
+++ server.js
@@ -8,6 +8,14 @@ require("./startup/cors")(app);
 require("./startup/db")();
 require("./startup/routes")(app);
 
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static("client/build"));
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+	});
+}
+
 const port = process.env.PORT || config.get("port");
 app.listen(port, () => {
 	console.log(`Server Started on ${port}`);
